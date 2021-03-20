6345e397b748f61d273a0d683f6c250ebb5f91e6
NTL <58549685+brad0520@users.noreply.github.com>
2021-03-20 23:20:07 +0900

    Update No2072.java

--- src/CodingTest/No2072.java
+++ src/CodingTest/No2072.java
@@ -1,27 +1,21 @@
-package swExpertAcademy;
+package javaStudy;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class No2072 {
 
-	public static void main(String[] args) throws Exception {
-
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-
-		for (int i = 1; i <= T; i++) {
-			int[] arr = new int[10];
-
-			for (int j = 0; j < arr.length; j++) {
-				arr[j] = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int sum = 0;
+			for (int j = 0; j < 10; j++) {
+				int num = sc.nextInt();
+				if (num % 2 != 0)
+					sum = sum + num;
 			}
-			int sum = Arrays.stream(arr).filter(j -> j % 2 == 1).sum();
-			System.out.println("#" + i + " " + sum);
-
+			System.out.println("#" + (i + 1) + " " + sum);
+			sum = 0;
 		}
-		
-		sc.close();
 	}
-
 }
