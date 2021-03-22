320caaa21018cbe90f90ec6513cc8af3d2216d59
NTL <58549685+brad0520@users.noreply.github.com>
2021-03-22 10:07:03 +0900

    Create No1933.java

---
+++ src/CodingTest/No1933.java
@@ -0,0 +1,22 @@
+package swExpertAcademy;
+
+import java.util.Scanner;
+
+public class No1933 {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A>B) {
+			System.out.print("A");
+		} else {
+			System.out.print("B");
+		}
+
+	}
+
+}
