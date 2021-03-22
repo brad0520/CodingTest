f49e81808effd0c06a75a95acc266084656ef470
NTL <58549685+brad0520@users.noreply.github.com>
2021-03-22 10:06:35 +0900

    Create No1549.java

---
+++ src/CodingTest/No1549.java
@@ -0,0 +1,32 @@
+package swExpertAcademy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class No1549 {
+
+	public static void main(String[] args) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		ArrayList<Integer> arr = new ArrayList<>();
+
+		for (int i = 1; i * i <= num; i++) {
+
+			if (num % i == 0) {
+				arr.add(i);
+				arr.add(num / i);
+			}
+		}
+
+		Collections.sort(arr);
+
+		for (Integer integer : arr) {
+			System.out.print(integer + " ");
+		}
+
+	}
+
+}
