d7a770006d95b54f32759b4ec19a31e0893877bb
NTL <wncks0303@naver.com>
2021-03-23 10:50:08 +0900

    .

--- src/swExpertAcademy/No2063.java
+++ src/swExpertAcademy/No2063.java
@@ -1,5 +1,6 @@
 package swExpertAcademy;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class No2063 {
@@ -7,11 +8,16 @@ public class No2063 {
 	public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
+        ArrayList <Integer> arr = new arrayList<>();
         
         int T = sc.nextInt();
- 
- 
+        
+        for (int i=0; i<T; i++) {
+        	arr[i] = sc.nextInt();
         }
+        
+        Arrays.sort(arr);
+        System.out.println(arr[T/2]);
 	}
 
 }
