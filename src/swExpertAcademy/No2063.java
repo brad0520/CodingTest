4a563d06497824e9eef6f2bc709aaa0276ab447f
NTL <wncks0303@naver.com>
2021-03-23 09:40:18 +0900

    No2063 update test

---
+++ src/swExpertAcademy/No2063.java
@@ -0,0 +1,17 @@
+package swExpertAcademy;
+
+import java.util.Scanner;
+
+public class No2063 {
+
+	public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+ 
+ 
+        }
+	}
+
+}
