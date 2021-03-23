43ddad68ded2a36559c1cd1ef4d6f9ad918b6d1d
NTL <58549685+brad0520@users.noreply.github.com>
2021-03-23 14:38:21 +0900

    No2071 update

--- src/swExpertAcademy/No2071.java
+++ src/swExpertAcademy/No2071.java
@@ -1,22 +1,21 @@
 package swExpertAcademy;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class No2063 {
+public class No2071 {
 
 	public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        int[] arr = new int[10];
         
         for (int i=1; i<=T; i++) {
+        	Double sum = 0.0;
         	for (int j=0; j<10; j++) {
-        		arr[j] = sc.nextInt();
+        		sum += sc.nextInt();
         	}
-        	Arrays.sort(arr);
-        	System.out.println("#" + i + " " + arr[9]);
+        	int avg = (int) Math.round(sum / 10);
+        	System.out.println("#" + i + " " + avg);
         }
         
 	}
